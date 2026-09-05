f2ac3e93f75b79643ee0d0d5bdc15eb7aab866a6
adarshkrrajak <adarshclg2312@gmail.com>
2026-09-05 19:11:38 +0530

    Practice continued

--- practice.c
+++ practice.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
-    printf("Hello World\n");
+    int a,b;
+    printf("Enter the value for a:");
+    scanf("%d",&a);
+    printf("Enter the value for b:");
+    scanf("%d",&b);
+    int power= pow(a,b);
+    printf("Value is %d",power);
+
     return 0;
 }
