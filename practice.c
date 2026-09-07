cd9f7c7a0fd12232efb3ae4034e8f899589e57b9
adarshkrrajak <adarshclg2312@gmail.com>
2026-09-07 11:11:28 +0530

    Grading system

--- practice.c
+++ practice.c
@@ -1,29 +1,96 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int day;
-    printf("Enter day (1-7): ");
-    scanf("%d",&day);
-    switch(day){
-        case 1: printf("Monday \n");
-                break;
-        case 2: printf("Tuesday \n");
-                break;
-        case 3: printf("Wednesday \n");
-                break;
-        case 4: printf("Thursday \n");     
-                break;
-        case 5: printf("Friday \n");
-                break;
-        case 6: printf("Saturday \n");
-                break;
-        case 7: printf("Sunday \n");
-                break;
-        default: printf("Invalid day \n");
+    int marks;
+    printf("Enter marks : ");
+    scanf("%d",&marks);
 
+    if(marks>=90 && marks<=100){
+        printf("A+ \n");
+    }
+    else if(marks>=80 && marks<90){
+        printf("A \n");
+    }
+    else if(marks>=70 && marks<80){
+        printf("B+ \n");
+    }
+    else if(marks>=60 && marks<70){
+        printf("B \n");
+    }
+    else if(marks>=50 && marks<60){
+        printf("C+ \n");
+    }
+    else if(marks>=40 && marks<50){
+        printf("C \n");
+    }
+    else if(marks>=30 && marks<40){
+        printf("D \n");
+    }
+    else if(marks>=0 && marks<30){
+        printf("F \n");
+    }
+    else{
+        printf("Invalid Marks \n");
     }
     return 0;
 }
+//     int marks;
+//     printf("Enter marks : ");
+//     scanf("%d",&marks);
+
+//     if(marks>30 && marks<=100){
+//         printf("You Passed the Exam \n");
+//     }
+//     else if(marks>=0 && marks<=30){
+//         printf("You Failed the Exam \n");
+//     }
+//     else if(marks>100 || marks<0){
+//         printf("Invalid Marks \n");
+//     }
+//     return 0;
+// }
+//     int number;
+//     printf("Enter number: ");
+//     scanf("%d",&number);
+
+//     if(number>=0){
+//         printf("Number is positive ");
+//         if(number%2==0){
+//             printf("and it's is even. \n");
+//         }
+//         else{
+//             printf("and it's is odd. \n");
+//         }
+//     }
+//     else{
+//         printf("Number is negative \n");
+//     }
+
+//     return 0;
+// }
+//     int day;
+//     printf("Enter day (1-7): ");
+//     scanf("%d",&day);
+//     switch(day){
+//         case 1: printf("Monday \n");
+//                 break;
+//         case 2: printf("Tuesday \n");
+//                 break;
+//         case 3: printf("Wednesday \n");
+//                 break;
+//         case 4: printf("Thursday \n");     
+//                 break;
+//         case 5: printf("Friday \n");
+//                 break;
+//         case 6: printf("Saturday \n");
+//                 break;
+//         case 7: printf("Sunday \n");
+//                 break;
+//         default: printf("Invalid day!\n");
+
+//     }
+//     return 0;
+// }
     // int age;
     // printf("Enter age: ");
     // scanf("%d",&age);
